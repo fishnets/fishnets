96c1f2969cedb466fc4e6c8b11531d0e27fb3317
Nokome Bentley <nokome.bentley@trophia.com>
2013-11-02 11:05:01 +0100

    add object.R

---
+++ object.R
@@ -0,0 +1,9 @@
+#' fishnets uses an object oriented approach based on R environments
+#' It is very similar to the approach described at http://www.lemnica.com/esotericR/Introducing-Closures/
+#' and used in the proto package (http://cran.r-project.org/web/packages/proto/index.html)
+#' Rather than introduce depenencies like proto, we just define this simple convieience function.
+object <- function(class.name){
+  self <- new.env()
+  class(self) <- class.name
+  self
+}
