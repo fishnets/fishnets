f0a2b057bccea6fbb77502a6fa474eba292c41b5
Nokome Bentley <nokome.bentley@trophia.com>
2013-11-03 12:03:29 +0100

    adds sampling to Taxonomic.imputer

--- taxonomic-imputer.R
+++ taxonomic-imputer.R
@@ -4,28 +4,35 @@
 #' at that taxonomic level.
 #' 
 #' @param predictand The name of the variable of being predicted
+#' @param transform A pair of from/to transformation functions e.g. c(log,exp)
 #' @param nmin Minimum sample size required for median/mode
-Taxonomic.imputer <- function(predictand,nmin){
+Taxonomic.imputer <- function(predictand,transform,nmin){
   self <- extend(Node,'Taxonomic.imputer')
   
   self$predictand <- predictand
+  self$transform <- transform
   self$predictors <- c('species','genus','family','order','class')
   self$nmin <- nmin
   
   self$fit <- function(data){
-    # Determine whether to use median or mode for defualt based on type of predictand
-    if(is.numeric(data[,self$predictand])){
-      defaults <- function(subset){    
+    # Determine the type of the predictand
+    self$type <- if(is.numeric(data[,self$predictand])) numeric else factor
+    # Determine type of default values to record based on type of predictand
+    if(is.numeric(self$type())){
+      defaults <- function(subset){  
+        values <- self$transform[[1]](subset[,self$predictand])
         list(
-          n = sum(!is.na(subset[,self$predictand])),
-          value = median(subset[,self$predictand],na.rm=T)
+          n = sum(!is.na(values)),
+          expect = mean(values,na.rm=T),
+          sd = sd(values,na.rm=T)
         )
       }
     } else {
       defaults <- function(subset){
+        values <- subset[,self$predictand]
         list(
-          n = sum(!is.na(subset[,self$predictand])),
-          value = names(which.max(table(subset[,self$predictand])))
+          n = sum(!is.na(values)),
+          expect = names(which.max(table(values)))
         )
       }
     }
@@ -40,23 +47,36 @@ Taxonomic.imputer <- function(predictand,nmin){
     )
   }
   
-  self$predict <- function(data){
-    # Try each taxonomic level until sufficient sample size for the
-    # variable is obtained
+  self$predict <- function(data,errors=F){
     preds <- NULL
+    # Loop through data rows
     for(row in 1:nrow(data)){
+      # Try each taxonomic level until sufficient sample size for the
+      # variable is obtained
+      default <- NULL
       for(level in c('species','genus','family','order','class','fish')){
-        group = as.character(data[row,level])
-        n <- tryCatch(self$defaults[[c(level,group,'n')]],error=function(error) 0)
-        if(!is.null(n)){
-          if(n>=self$nmin){
-            preds = c(preds,self$defaults[[c(level,group,'value')]][1])
+        group <- as.character(data[row,level])
+        defaults <- self$defaults[[c(level,group)]]
+        if(!is.null(defaults)){
+          if(defaults$n>=self$nmin){
+            default <- defaults
             break
           }
         }
-      } 
+      }
+      if(errors) return(defaults)
+      else preds = c(preds,self$transform[[2]](default$expect))
     }
     preds
+  }
+  
+  self$sample <- function(data,samples=1){
+    # Currently, only provides sampling for numeric predictands
+    if(!is.numeric(self$type())) stop('Sorry, sampling of non-numeric predictands is not implemented yet')
+    # Get prediction
+    pred <- self$predict(data,errors=T)
+    # Randomly sample and inverse transform
+    self$transform[[2]](rnorm(samples,pred$expect,pred$sd))
   }
   
   self
