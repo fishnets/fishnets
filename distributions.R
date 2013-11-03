629e8efd69b585ee370ab142b4e4085a58709c95
Nokome Bentley <nokome.bentley@trophia.com>
2013-11-03 17:37:27 +0100

    adds Trapezoid distribution

--- distributions.R
+++ distributions.R
@@ -1,8 +1,10 @@
+require(trapezoid)
+
 Distribution <- function(rfunc,...){
   self <- object('Dist')
   
-  self$random <- function(){
-    rfunc(1,...)
+  self$random <- function(n=1){
+    rfunc(n,...)
   }
   
   self
@@ -21,6 +23,8 @@ Uniform <- function(...) extend(Distribution,'Uniform',runif,...)
 Normal <- function(...) extend(Distribution,'Normal',rnorm,...)
 Lognormal <- function(...) extend(Distribution,'Lognormal',rlnorm,...)
 Beta <- function(...) extend(Distribution,'Beta',rbeta,...)
+Trapezoid <- function(...) extend(Distribution,'Trapezoid',rtrapezoid,...)
+Triangle <- function(min,mode,max) extend(Distribution,'Triangle',rtrapezoid,min=min,mode1=mode,mode2=mode,max=max)
 
 dists <- function(...){
   dists <- lapply(as.list(match.call(expand.dots=T)[-1]),eval)
